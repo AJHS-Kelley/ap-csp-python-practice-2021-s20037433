2491ea2095d8e97834f6f780abf1dddd0d813242
Christopher Thomas <90337286+s20037433@users.noreply.github.com>
2021-10-29 13:22:11 -0400

    Update variables_practice.py
    I ran my int float string variables

--- variables/variables_practice.py
+++ variables/variables_practice.py
@@ -10,3 +10,12 @@ my_string = "I had a really good day at school."
 print(my_int)
 print(my_float)
 print(my_string)
+
+my_int = input("Please type the number of Shoes you own. \n")
+print(my_int)
+
+my_float= input("What is your highschool gpa. \n")
+print(my_f)
+
+my_string = input("type your first name. \n")
+print(my_string)
