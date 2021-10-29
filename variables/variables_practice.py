4f0ad5a3a649f4dce9a28aeca048c782ccb37e6e
Christopher Thomas <90337286+s20037433@users.noreply.github.com>
2021-10-29 13:03:09 -0400

    Version 0.1
    Added 3 variables of integers, float, and string.

---
+++ variables/variables_practice.py
@@ -0,0 +1,8 @@
+# Variables Practice, Christopher Thomas, 10/29/21 12:49pm, version 0.1 
+
+
+# Basic Variables 
+
+my_int = 12 # INTERGERS, any +/- whole number and 0.
+my_float = 1.234567 # FLOATING POINT, any +/- number with a decimal.
+my_string = "I had a really good day at school."
